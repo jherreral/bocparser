d42f2c0824e97e705bb493cc3261c5426a0c49aa
jherreral <33136201+jherreral@users.noreply.github.com>
2023-04-19 22:09:00 -0400

    Added full description comparison.

--- bocparser.py
+++ bocparser.py
@@ -1,10 +1,29 @@
 import pandas as pd
 
-# Specify the range of cells to read
+categories = {"Delivery":["RAPPI RAPPI ** COMPRAS","KUSHKI *JUSTO CHILE COMPRAS","MARKET CONDELL           SANTIAGO     CL"],"Supermercado":["JUMBO BILBAO COMPRAS"]}
+
+def get_category(description):
+    for key,value in categories.items():
+        if description in value:
+            return key
+    return None
+
+
+def categorizeMovements(df:pd.DataFrame):
+    mask = df.apply(lambda row: get_category(row['Descripción']),axis=1)
+    df['Rubro'] = mask
+
+# Read the bank movements from an Excel file
 cell_range = 'B:K'
 rows_to_skip=17
-# Read the range of cells into a Pandas DataFrame
 df = pd.read_excel('saldo.xls', skiprows=rows_to_skip,usecols=cell_range)
-df.drop(axis=1, index=3,inplace=True)
+
+# Clean up table
+df.rename(columns={"Unnamed: 10":"Monto"},inplace=True)
+df.drop(columns=['Unnamed: 3','Unnamed: 5','Unnamed: 9','Monto ($)'],inplace=True)
+
+# Categorize movements according to type
+categorizeMovements(df)
+
 # Print the DataFrame
 print(df)
